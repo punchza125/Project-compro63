a8cd6bd34a0042f199f65d4b5b1138854b29e9eb
punchza125 <71113084+punchza125@users.noreply.github.com>
2021-05-05 23:46:19 +0700

    Update projectcur1.c

--- projectcur1.c
+++ projectcur1.c
@@ -12,24 +12,53 @@ int main(){
 	printf("Enter the currency you want to exchange:");
     scanf("%s", currency);
     printf("How many?");
-    scanf("%f", money);
+    scanf("%f", &money);
     
     
+    //mcurrency
 
-	
-	
     if(strcmp(mcurrency, "USD") == 0){
         bpoint = 31.0177;
     }
-	if(strcmp(currency, "EUR") == 0){
+    else if(strcmp(mcurrency, "BTH") == 0){
+        bpoint = 999;
+    }
+    else if(strcmp(mcurrency, "GBP") == 0){
+        bpoint = 42.9129;
+    }
+    else if(strcmp(mcurrency, "EUR") == 0){
+        bpoint = 37.0815;
+    }
+    else if(strcmp(mcurrency, "JPY") == 0){
+        bpoint = 28.1671/100;
+    }
+    else if(strcmp(mcurrency, "HKD") == 0){
+        bpoint = 3.9728;
+    }
+
+    
+    //currency
+    
+    
+    if(strcmp(currency, "USD") == 0){
+        spoint = 31.3553;
+    }
+	else if(strcmp(currency, "EUR") == 0){
 		spoint = 37.8612;
+	}
+	else if(strcmp(currency, "GBP") == 0){
+	    spoint = 43.8152;
 	}
 	
 	
+	//calculate
 	
-	
-	
-	printf("%.2f\n", bpoint);
-	printf("%.2f\n", spoint);
+	printf("%f\n", bpoint);
+	printf("%f\n", spoint);
+	if(bpoint==999){
+	    printf("%f\n", money/spoint);
+	}
+	else{
+	printf("%.2f\n", (bpoint*money)/spoint);}
 	return 0;
 }
