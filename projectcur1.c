8e9e8d43ce002f894b08b3f953828fcc036fc867
punchza125 <71113084+punchza125@users.noreply.github.com>
2021-05-05 23:12:39 +0700

    Add files via upload

---
+++ projectcur1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+	float money, spoint, bpoint;
+
+	char mcurrency[4], currency[4];
+	
+	printf("Enter your currency:");
+	scanf("%s", mcurrency);
+	printf("Enter the currency you want to exchange:");
+    scanf("%s", currency);
+    printf("How many?");
+    scanf("%f", money);
+    
+    
+
+	
+	\\\\
+    if(strcmp(mcurrency, "USD") == 0){
+        bpoint = 31.0177;
+    }
+	if(strcmp(currency, "EUR") == 0){
+		spoint = 37.8612;
+	}
+	
+	
+	
+	
+	
+	printf("%.2f\n", bpoint);
+	printf("%.2f\n", spoint);
+	return 0;
+}
